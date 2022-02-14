320a19f87c5bab1f356526c45f12932c0fe68bd7
Esther J Iyege <77330706+Faithesther20@users.noreply.github.com>
2022-02-14 18:47:26 +0100

    added component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,5 @@
 import React from "react";
-
-import emojipedia from "../emojipedia";
-import Card from "./Card";
-
-function emojiList(emojies) {
-  return (
-    <Card
-      key={emojies.id}
-      emojiName={emojies.emoji}
-      title={emojies.name}
-      data={emojies.meaning}
-    />
-  );
-}
+import Entry from "./Entry";
 
 function App() {
   return (
@@ -20,8 +7,7 @@ function App() {
       <h1>
         <span>emojipedia</span>
       </h1>
-
-      <dl className="dictionary">{emojipedia.map(emojiList)}</dl>
+      <Entry />
     </div>
   );
 }
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,9 +1,21 @@
+import React from "react";
 
+import emojipedia from "../emojipedia";
+import Card from "./Card";
 
-// function Entry(){
-//   return(
-   
-   
-//   );
-// }
-// export default Entry;
+// map
+function emojiList(emojies) {
+  return (
+    <Card
+      key={emojies.id}
+      emojiName={emojies.emoji}
+      title={emojies.name}
+      data={emojies.meaning}
+    />
+  );
+}
+
+function Entry() {
+  return <dl className="dictionary">{emojipedia.map(emojiList)}</dl>;
+}
+export default Entry;
