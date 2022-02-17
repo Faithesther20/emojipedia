be791136e4b1a9aa5d0bf671e3c25fac63270cf8
Esther J Iyege <77330706+Faithesther20@users.noreply.github.com>
2022-02-17 15:46:32 +0100

    d

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 
-function Card(props){
-   return(
+function Card(props) {
+  return (
     <div className="term">
-    <dt>
-      <span className="emoji" role="img" aria-label="Tense Biceps">
-       {props.emojiName}
-      </span>
-      <span>{props.title}</span>
-    </dt>
-    <dd>
-      {props.data}
-    </dd>
-  </div>
-   );
-   
+      <dt>
+        <span className="emoji" role="img" aria-label="Tense Biceps">
+          {props.emojiNam}
+        </span>
+        <span>{props.title}</span>
+      </dt>
+      <dd>{props.data}</dd>
+    </div>
+  );
 }
 export default Card;
